3920ae5b196b0c41d6c60573e71d581736768b47
NabilaSyaidaRamdani <129932731+NabilaSyaidaRamdani@users.noreply.github.com>
2025-08-16 19:06:37 +0700

    Update nabila_syaida_hacktiv8.py

--- nabila_syaida_hacktiv8.py
+++ nabila_syaida_hacktiv8.py
@@ -2,9 +2,3 @@ import streamlit as st
 
 # Ambil token dari secret
 token = st.secrets["GITHUB"]["TOKEN"]
-credential = AzureKeyCredential(token)
-
-client = ChatCompletionsClient(
-    endpoint="https://models.github.ai/inference",
-    credential=credential
-)
