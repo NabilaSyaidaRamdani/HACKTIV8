6f4c3e8764e219393ee6c83996588fddb9159f42
NabilaSyaidaRamdani <129932731+NabilaSyaidaRamdani@users.noreply.github.com>
2025-08-16 18:57:43 +0700

    Update nabila_syaida_hacktiv8.py

--- nabila_syaida_hacktiv8.py
+++ nabila_syaida_hacktiv8.py
@@ -1,10 +1,6 @@
 import streamlit as st
 
 st.title('Nabila Syaida')
-import streamlit as st
-from azure.core.credentials import AzureKeyCredential
-from azure.ai.inference import ChatCompletionsClient
-from azure.ai.inference.models import UserMessage
 
 # Ambil token dari Streamlit Secrets
 token = st.secrets["GITHUB_TOKEN"]
