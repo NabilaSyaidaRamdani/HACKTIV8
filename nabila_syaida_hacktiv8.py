6e62c556526a736cf262852ab6aed975105d0b9b
NabilaSyaidaRamdani <129932731+NabilaSyaidaRamdani@users.noreply.github.com>
2025-08-16 23:44:04 +0700

    Update nabila_syaida_hacktiv8.py

--- nabila_syaida_hacktiv8.py
+++ nabila_syaida_hacktiv8.py
@@ -3,17 +3,16 @@ from huggingface_hub import InferenceClient
 
 st.title("Chat dengan Model Hugging Face")
 
-# Ambil token dari secrets
+# Ambil token
 hf_token = st.secrets["TOKEN"]
 
-# Pilih model Hugging Face (gratis & open)
-# Kamu bisa ganti model_id sesuai kebutuhan, misalnya "google/flan-t5-base"
+# Gunakan model yang support text-generation
 client = InferenceClient(model="gpt2", token=hf_token)
 
 # Input pengguna
 user_input = st.text_input("Tulis pertanyaan kamu:")
 
 if user_input:
-    response = client.text_generation(user_input, max_new_tokens=200)
+    response = client.text_generation(user_input, max_new_tokens=100)
     st.markdown("**Jawaban Model:**")
     st.write(response)
